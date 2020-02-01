387cffbc60537b7b0f55002cb793dade1a33475a
Kevin Villalobos <kevinv15v@gmail.com>
2020-02-01 13:01:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,7 +54,7 @@ class Song
   end
   
   def self.create_from_filename(name)
-  song
+  
     song = self.new
     song.name = (name.split(" - ")[1].chomp(".mp3"))
     song.artist_name = (name.split(" - ")[0])
